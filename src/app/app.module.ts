cbdf26fea3f8d40ceb4ffae9f7502f745807a647
geraldineac <87266809+geraldineac@users.noreply.github.com>
2022-11-10 16:52:38 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA} from `@angular/core`;
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
